8db4c42c5e52a0711ee6d26f0c5549b998cf60a3
Ryan Harrington <harrington.ryana@gmail.com>
2019-08-02 08:11:51 -0400

    Get fourteener by id

--- FourteenersREST/src/main/java/com/skilldistillery/fourteeners/controllers/FourteenerController.java
+++ FourteenersREST/src/main/java/com/skilldistillery/fourteeners/controllers/FourteenerController.java
@@ -2,6 +2,9 @@ package com.skilldistillery.fourteeners.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +23,21 @@ public class FourteenerController {
 	}
 	
 	@GetMapping("fourteeners")
-	public List<Fourteener> getAllFourteeners() {
-		return svc.index();
+	public List<Fourteener> getAllFourteeners(HttpServletRequest req, HttpServletResponse resp) {
+		List<Fourteener> fourteeners = svc.index();
+		if (fourteeners == null) {
+			resp.setStatus(404);
+		}
+		return fourteeners;
+	}
+	
+	@GetMapping("fourteeners/{id}")
+	public Fourteener getFourteener(@PathVariable("id") int id, HttpServletRequest req, HttpServletResponse resp) {
+		Fourteener mtn = svc.show(id);
+		if (mtn == null) {
+			resp.setStatus(404);
+		}
+		
+		return mtn;
 	}
 }
